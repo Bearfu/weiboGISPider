b3bb7bf205c4bdfdf35a8578190182b68293394e
傅 喆 <303761829@qq.com>
2019-10-14 19:04:07 +0800

    创建Demo 编写数据库表结构文档

---
+++ DaoInfo.md
@@ -0,0 +1,51 @@
+# 数据表结构说明文档
+
+
+
+## 微博数据表
+    该表保存的是获取到的微博相关数据
+    
+    
+|名称|类型|长度|允许为空|备注|
+|:---|:---|:---|:---|:---|
+|id|bigint|20|不允许|主键ID(自增)|
+|msg_id|bigint|20|允许|微博ID 唯一索引|
+|address|varchar|255|允许|微博发布地址|
+|content|mediumtext|0|允许|文章的内容|
+|msg_time|varchar|255|允许|微博发布时间|
+|msg_timestamp|timestamp|0|允许|微博发布时间戳|
+|tools|varchar|255|允许|微博发布工具|
+|transmi_count|int|10|允许|转发数|
+|comment_count|int|10|允许|评论数|
+|praise_count|int|10|允许|点赞数|
+|lng|float|0|允许|文章的创建时间|
+|lat|float|0|允许|文章的更新时间|
+|created_at|timestamp|0|允许|数据创建时间|
+|updated_at|timestamp|0|允许|数据更新时间|
+    
+ 
+
+### 建表语句
+
+````
+    DROP TABLE IF EXISTS `weiboGis`;
+    CREATE TABLE `weiboGis` (
+      `id` bigint(20) NOT NULL AUTO_INCREMENT,
+      `msg_id` bigint(20) DEFAULT NULL,
+      `address` varchar(255) DEFAULT NULL,
+      `content` mediumtext,
+      `msg_time` varchar(255) DEFAULT NULL,
+      `msg_timestamp` datetime DEFAULT NULL,
+      `tools` varchar(255) DEFAULT NULL,
+      `transmi_count` int(10) DEFAULT NULL,
+      `comment_count` int(10) DEFAULT NULL,
+      `praise_count` int(10) DEFAULT NULL,
+      `lng` float DEFAULT NULL,
+      `lat` float DEFAULT NULL,
+      `created_at` datetime DEFAULT NULL,
+      `updated_at` datetime DEFAULT NULL,
+      PRIMARY KEY (`id`) USING BTREE
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+    
+    SET FOREIGN_KEY_CHECKS = 1;
+```
---
+++ Demo.py
@@ -0,0 +1,44 @@
+import requests
+import urllib
+
+SelectCode = "北京大雨"  # 关键字
+
+Regionnum = "4"  # 东城区代码
+Start_time = "2012-07-21-2"  # 起始时间
+End_time = "2012-07-22-23"  # 结束时间
+page = 2
+
+
+def doubleEncode(key):
+    try:
+        return urllib.parse.quote(urllib.parse.quote(key))
+    except Exception:
+        return ""
+
+
+URL = "https://s.weibo.com/weibo/{}?q={}&&region=custom:11:{}&typeall=1&suball=1&timescope=custom:{}:{}&Refer=g".format(
+    doubleEncode(SelectCode), SelectCode, Regionnum, Start_time, End_time
+)
+
+
+
+
+if __name__ == '__main__':
+    url = "https://s.weibo.com/weibo/{}?q={}&&region=custom:11:{}&typeall=1&suball=1&timescope=custom:{}:{}&Refer=g&page={}".format(
+        doubleEncode(SelectCode), SelectCode, Regionnum, Start_time, End_time,page
+    )
+    headers = {
+        'Connection': "keep-alive",
+        'Upgrade-Insecure-Requests': "1",
+        'Sec-Fetch-Mode': "navigate",
+        'Sec-Fetch-User': "?1",
+        'Sec-Fetch-Site': "same-origin",
+        'Accept-Encoding': "gzip, deflate, br",
+        'Accept-Language': "zh-CN,zh;q=0.9,en;q=0.8",
+        'Cookie': "SINAGLOBAL=9567883550528.377.1569808672284; login_sid_t=de9791f791e69a37a9b20be943761e17; cross_origin_proto=SSL; _s_tentry=www.baidu.com; Apache=5326827333880.572.1570617980182; ULV=1570617980196:2:1:1:5326827333880.572.1570617980182:1569808672294; SSOLoginState=1570617981; un=chengqigu@wallstreetcn.com; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9W5yo3qLH_A-Z1xIXjnFVHa35JpX5KMhUgL.Fo-fSKz41hzESKz2dJLoIpRLxK.L1h5L1h.LxKnL1hzLBK2LxK-LB--L1-xW-85t; SUHB=0d4Og_CYjdgbhW; ALF=1602584476; SCF=AlKv9XO_kn5oN9LEpTD5dfRHwvgf9Y6efvgGH20icb--RTuX05srXx2AYecaHBr3z1ZHHjCVmaUzWthTfO3_aTU.; SUB=_2A25woDxPDeRhGeNL7lAY-CzOzj6IHXVT1CqHrDV8PUNbmtBeLWbhkW9NSOkvcodneQeizR2Siw80B7BykN5YyxMj; UOR=www.techug.com,widget.weibo.com,login.sina.com.cn; webim_unReadCount=%7B%22time%22%3A1571048489304%2C%22dm_pub_total%22%3A0%2C%22chat_group_client%22%3A0%2C%22allcountNum%22%3A97%2C%22msgbox%22%3A0%7D; WBStorage=384d9091c43a87a5|undefined",
+        'cache-control': "no-cache",
+    }
+
+    response = requests.request("GET", url, headers=headers)
+
+    print(response.text)
