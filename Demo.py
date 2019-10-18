fb1b1d54ebe331af6dcc78ab08307f5477b6c5d2
傅 喆 <303761829@qq.com>
2019-10-18 17:33:02 +0800

    跑通逻辑

--- Demo.py
+++ Demo.py
@@ -6,7 +6,7 @@ SelectCode = "北京大雨"  # 关键字
 Regionnum = "4"  # 东城区代码
 Start_time = "2012-07-21-2"  # 起始时间
 End_time = "2012-07-22-23"  # 结束时间
-page = 2
+page = 12
 
 
 def doubleEncode(key):
@@ -20,25 +20,27 @@ URL = "https://s.weibo.com/weibo/{}?q={}&&region=custom:11:{}&typeall=1&suball=1
     doubleEncode(SelectCode), SelectCode, Regionnum, Start_time, End_time
 )
 
-
-
-
 if __name__ == '__main__':
     url = "https://s.weibo.com/weibo/{}?q={}&&region=custom:11:{}&typeall=1&suball=1&timescope=custom:{}:{}&Refer=g&page={}".format(
-        doubleEncode(SelectCode), SelectCode, Regionnum, Start_time, End_time,page
+        doubleEncode(SelectCode), SelectCode, Regionnum, Start_time, End_time, page
     )
     headers = {
         'Connection': "keep-alive",
+        'Cache-Control': "max-age=0",
         'Upgrade-Insecure-Requests': "1",
+        'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36",
         'Sec-Fetch-Mode': "navigate",
         'Sec-Fetch-User': "?1",
-        'Sec-Fetch-Site': "same-origin",
+        'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+        'Sec-Fetch-Site': "none",
         'Accept-Encoding': "gzip, deflate, br",
         'Accept-Language': "zh-CN,zh;q=0.9,en;q=0.8",
-        'Cookie': "SINAGLOBAL=9567883550528.377.1569808672284; login_sid_t=de9791f791e69a37a9b20be943761e17; cross_origin_proto=SSL; _s_tentry=www.baidu.com; Apache=5326827333880.572.1570617980182; ULV=1570617980196:2:1:1:5326827333880.572.1570617980182:1569808672294; SSOLoginState=1570617981; un=chengqigu@wallstreetcn.com; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9W5yo3qLH_A-Z1xIXjnFVHa35JpX5KMhUgL.Fo-fSKz41hzESKz2dJLoIpRLxK.L1h5L1h.LxKnL1hzLBK2LxK-LB--L1-xW-85t; SUHB=0d4Og_CYjdgbhW; ALF=1602584476; SCF=AlKv9XO_kn5oN9LEpTD5dfRHwvgf9Y6efvgGH20icb--RTuX05srXx2AYecaHBr3z1ZHHjCVmaUzWthTfO3_aTU.; SUB=_2A25woDxPDeRhGeNL7lAY-CzOzj6IHXVT1CqHrDV8PUNbmtBeLWbhkW9NSOkvcodneQeizR2Siw80B7BykN5YyxMj; UOR=www.techug.com,widget.weibo.com,login.sina.com.cn; webim_unReadCount=%7B%22time%22%3A1571048489304%2C%22dm_pub_total%22%3A0%2C%22chat_group_client%22%3A0%2C%22allcountNum%22%3A97%2C%22msgbox%22%3A0%7D; WBStorage=384d9091c43a87a5|undefined",
-        'cache-control': "no-cache",
+        'Cookie': "SINAGLOBAL=9567883550528.377.1569808672284; un=chengqigu@wallstreetcn.com; wvr=6; login_sid_t=c730c63997f8c1fbf6960d47c6ae13f4; cross_origin_proto=SSL; _s_tentry=passport.weibo.com; UOR=www.techug.com,widget.weibo.com,localhost; Apache=7701813525170.181.1571381327292; ULV=1571381327313:3:2:1:7701813525170.181.1571381327292:1570617980196; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9W5yo3qLH_A-Z1xIXjnFVHa35JpX5K2hUgL.Fo-fSKz41hzESKz2dJLoIpRLxK.L1h5L1h.LxKnL1hzLBK2LxK-LB--L1-xW-85t; SSOLoginState=1571381332; ALF=1602917341; SCF=AlKv9XO_kn5oN9LEpTD5dfRHwvgf9Y6efvgGH20icb--QWYVl8lg65e1raabzxIXO6j6gUS1vobM3F5cDcDwekI.; SUB=_2A25wrRAPDeRhGeNL7lAY-CzOzj6IHXVT2wbHrDV8PUNbmtBeLWr4kW9NSOkvckBPXa-UxJetCyMUkttN_iYm6bxR; SUHB=0102xsCr3dUnQr; webim_unReadCount=%7B%22time%22%3A1571381441666%2C%22dm_pub_total%22%3A0%2C%22chat_group_client%22%3A0%2C%22allcountNum%22%3A98%2C%22msgbox%22%3A0%7D; WBStorage=384d9091c43a87a5|undefined",
+        'Host': "s.weibo.com",
+        'cache-control': "no-cache"
     }
-
+    url = "https://s.weibo.com/weibo/%25E5%258C%2597%25E4%25BA%25AC%25E5%25A4%25A7%25E9%259B%25A8?q=北京大雨&region=custom:11:1&typeall=1&suball=1&timescope=custom:2012-07-20-10:2012-07-20-20&Refer=g"
+    print(url)
     response = requests.request("GET", url, headers=headers)
 
     print(response.text)
--- config.py
+++ config.py
@@ -23,38 +23,14 @@ def get_mysql_connection():
     :return: Object
     """
 
-    mysql_config = CONFIG['STORAGES']['MYSQL']
+    connection = pymysql.connect(host="127.0.0.1",
+                                 user='root',
+                                 port=3306,
+                                 password="134679852",
+                                 db='weibo',
+                                 charset="utf8",
+                                 )
 
-    if CONFIG['STORAGES']['MYSQL'].get('LOCAL_JUMP'):
-        jump_config = CONFIG['STORAGES']['MYSQL']['LOCAL_JUMP']
-        # client = paramiko.SSHClient()
-        # client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        # client.load_system_host_keys()
-        # client.connect(jump_config['SSH_IP'], username=jump_config['SSH_USER'])
-        # transport = client.get_transport()
-        # channel = transport.open_channel('direct-tcpip', (mysql_config['HOST'], mysql_config['PORT']),('localhost', 4144))
-        # connection = pymysql.connect(database=mysql_config['DB'],
-        #                              user=mysql_config['USER'],
-        #                              port=mysql_config['PORT'],
-        #                              passwd=mysql_config['PASS'],
-        #                              charset=mysql_config['CHARSET'],
-        #                              cursorclass=pymysql.cursors.DictCursor,
-        #                              defer_connect=True)
-        # connection.connect(channel)
-
-        pool = PooledDB(pymysql, 5, **jump_config)
-        cnx = pool.connection()
-        return cnx
-
-
-    else:
-        connection = pymysql.connect(host=mysql_config['HOST'],
-                                     user=mysql_config['USER'],
-                                     port=mysql_config['PORT'],
-                                     password=mysql_config['PASS'],
-                                     db=mysql_config['DB'],
-                                     charset=mysql_config['CHARSET'],
-                                     cursorclass=pymysql.cursors.DictCursor)
     return connection
 
 
